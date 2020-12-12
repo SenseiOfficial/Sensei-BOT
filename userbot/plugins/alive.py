3facedd09c2af3f811b963abe8d3675aaddf0e84
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-13 01:33:12 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,7 +7,7 @@ from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Senseibot user"
 PM_IMG = "https://telegra.ph/file/023df30443809b46e0843.mp4"
 pm_caption = "`🔥⚠️HΞЯΞ CФMΞS ΓHΞ G.Ф.Д.Γ. SΞИSΞIБФΓ⚠️🔥\n\n"
-pm_caption += "`🛡️𝕾𝖞𝖘𝖙𝖊𝖒 𝖘𝖙𝖆𝖙𝖚𝖘🛡️`\n"
+pm_caption += "`    🛡️𝕾𝖞𝖘𝖙𝖊𝖒 𝖘𝖙𝖆𝖙𝖚𝖘🛡️  `\n"
 pm_caption += "`🔱ᴛᴇʟᴇᴛʜᴏɴ🔱       :` **6.0.9**\n` ♾Python:` **3.8.5**\n"
 pm_caption += "`⚠️ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ⚠️`: `↼🄼🄰🅂🅃🄴🅁⇀`\n"
 pm_caption += "`☯ѕєиѕєιвσт☯      : `2.14`\n"
