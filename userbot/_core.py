32860835076e3ed8e741650c1c744c072dcc9ecf
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-13 01:01:55 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -1,86 +1,94 @@
-from userbot import bot
-from telethon import events
-from userbot.utils import command, remove_plugin, load_module
-from var import Var
-import importlib
-from pathlib import Path
-from userbot import LOAD_PLUG
-import sys
+#kanged from hellbot by @kraken_the_badass
+#thanks to him for this
+
 import asyncio
-import traceback
 import os
-import userbot.utils
 from datetime import datetime
+from pathlib import Path
+from telethon.tl.types import InputMessagesFilterDocument
+from userbot.utils import admin_cmd, load_module, remove_plugin, edit_or_reply
+from userbot import ALIVE_NAME
+from userbot import bot
 
 DELETE_TIMEOUT = 5
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Senseibot user"
 
-@command(pattern="^.install", outgoing=True)
+
+@bot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
+async def send(event):
+    if event.fwd_from:
+        return
+    kraken = bot.uid
+    message_id = event.message.id
+    input_str = event.pattern_match.group(1)
+    the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
+    if os.path.exists(the_plugin_file):
+        start = datetime.now()
+        pro = await event.client.send_file(
+            event.chat_id,
+            the_plugin_file,
+            force_document=True,
+            allow_cache=False,
+            reply_to=message_id,
+        )
+        end = datetime.now()
+        time_taken_in_ms = (end - start).seconds
+        await pro.edit(
+            f"**〖 Plugin name 〗➟** `{input_str}`\n**〖 Uploaded in 〗➟** `{time_taken_in_ms} secs`\n**〖 Uploaded by〗➟** [{DEFAULTUSER}](tg://user?id={kraken})\n"
+        )
+        await asyncio.sleep(DELETE_TIMEOUT)
+        await event.delete()
+    else:
+        await edit_or_reply(event, "File not found.....u noob check whethe ryou typed correct or not")
+
+@bot.on(admin_cmd(pattern="install"))
 async def install(event):
     if event.fwd_from:
         return
     if event.reply_to_msg_id:
         try:
-            downloaded_file_name = await event.client.download_media(  # pylint:disable=E0602
-                await event.get_reply_message(),
-                "userbot/plugins/"  # pylint:disable=E0602
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "userbot/plugins/",  # pylint:disable=E0602
+                )
             )
             if "(" not in downloaded_file_name:
                 path1 = Path(downloaded_file_name)
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
-                await event.edit("Installed Plugin `{}`".format(os.path.basename(downloaded_file_name)))
+                await event.edit(
+                    "Plugin successfully installed\n @senseiMAxprojects `{}`".format(
+                        os.path.basename(downloaded_file_name)
+                    )
+                )
             else:
                 os.remove(downloaded_file_name)
-                await event.edit("Errors! This plugin is already installed/pre-installed Hmm As Usual My Owner @Sensei_nex Is Some Mental.")
+                await event.edit(
+                    "**Error!**\nPlugin cannot be installed!\n Or may have been pre-installed."
+                )
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
             os.remove(downloaded_file_name)
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
-@command(pattern="^.send (?P<shortname>\w+)$", outgoing=True)
-async def send(event):
-    if event.fwd_from:
-        return
-    message_id = event.message.id
-    input_str = event.pattern_match["shortname"]
-    the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
-    start = datetime.now()
-    await event.client.send_file(  # pylint:disable=E0602
-        event.chat_id,
-        the_plugin_file,
-        force_document=True,
-        allow_cache=False,
-        reply_to=message_id
-    )
-    end = datetime.now()
-    time_taken_in_ms = (end - start).seconds
-    await event.edit("Uploaded {} in {} seconds".format(input_str, time_taken_in_ms))
-    await asyncio.sleep(DELETE_TIMEOUT)
-    await event.delete()
-
-@command(pattern="^.unload (?P<shortname>\w+)$", outgoing=True)
+@bot.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
 async def unload(event):
     if event.fwd_from:
         return
     shortname = event.pattern_match["shortname"]
     try:
         remove_plugin(shortname)
-        await event.edit(f"Unloaded {shortname} successfully")
+        await event.edit(f"Successfully unloaded {shortname}")
     except Exception as e:
-        await event.edit("Successfully unload {shortname}\n{}".format(shortname, str(e)))
+        await event.edit(
+            "Successfully unloaded {shortname}\n{}".format(
+                shortname, str(e)
+            )
+        )
 
-@command(pattern="^.load (?P<shortname>\w+)$", outgoing=True)
+
+@bot.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
 async def load(event):
-    if event.fwd_from:
-        return
-    shortname = event.pattern_match["shortname"]
-    try:
-        try:
-            remove_plugin(shortname)
-        except:
-            pass
-        load_module(shortname)
-        await event.edit(f"Successfully loaded {shortname}")
-    except Exception as e:
-        await event.edit(f"Could not load {shortname} because of the following error.\n{str(e)}")
+
