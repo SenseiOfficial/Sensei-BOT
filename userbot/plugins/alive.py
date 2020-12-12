42fd4bbd7adb340578814766b4bda5ea6f8a27cb
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-13 02:09:10 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,14 +7,14 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Senseibot user"
 PM_IMG = "https://telegra.ph/file/023df30443809b46e0843.mp4"
-pm_caption = "`⚠️HΞЯΞ CФMΞS ΓHΞ G.Ф.Д.Γ. SΞИSΞIБФΓ⚠️\n"
+pm_caption = "`⚠️HΞЯΞ CФMΞS ΓHΞ G.Ф.Д.Γ. [SΞИSΞIБФΓ](https://github.com/SenseiOfficial/Sensei-BOT)⚠️\n"
 pm_caption += "`                     🛡️𝕾𝖞𝖘𝖙𝖊𝖒 𝖘𝖙𝖆𝖙𝖚𝖘🛡️  `\n\n"
-pm_caption += "`🔱ᴛᴇʟᴇᴛʜᴏɴ🔱       :` **6.0.9**\n` ♾Python:` **3.8.5**\n"
+pm_caption += "`🔱ᴛᴇʟᴇᴛʜᴏɴ🔱       :`**6.0.9**\n` ♾Python:` **3.8.5**\n"
 pm_caption += "`⚠️ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ⚠️`: `↼🄼🄰🅂🅃🄴🅁⇀`\n"
-pm_caption += "`☯ѕєиѕєιвσт☯      : `2.14`\n"
-pm_caption += f"♾ᴍʏ ʟɪᴇɢᴇ♾       : {DEFAULTUSER} \n"
-pm_caption += "✵ᴍʏ ᴅᴇᴠ✵          : [This nub](https://t.me/sensei_nex)\n\n"
-pm_caption += "𝕎𝕒𝕟𝕟𝕒 𝕙𝕒𝕧𝕖 𝕪𝕠𝕦𝕣 𝕠𝕨𝕟 [ѕєиѕєιвσт](https://github.com/SenseiOfficial/Sensei-BOT)\n"
+pm_caption += "`⚜️ѕєиѕєιвσт⚜️      :`2.17`\n"
+pm_caption += f"♾ᴍʏ ʟɪᴇɢᴇ♾       :{DEFAULTUSER} \n"
+pm_caption += "✵ᴍʏ ᴅᴇᴠ✵          :[This nub](https://t.me/sensei_nex)\n\n"
+pm_caption += "✮License✮         :[SΞИSΞIБФΓ](https://github.com/SenseiOfficial/Sensei-BOT/blob/master/LICENSE)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
