500dc0cb832d2a7485422d5e5d933e669fde7bc7
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-13 03:21:39 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,12 +9,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Senseibot user"
 PM_IMG = "https://telegra.ph/file/023df30443809b46e0843.mp4"
 pm_caption = "`⚠️HΞЯΞ CФMΞS ΓHΞ G.Ф.Д.Γ. SΞИSΞIБФΓ⚠️\n"
 pm_caption += "`                   【🛡️ 𝕾𝖞𝖘𝖙𝖊𝖒 𝖘𝖙𝖆𝖙𝖚𝖘 🛡️】  `\n\n"
-pm_caption += "`🔱─ᴛᴇʟᴇᴛʜᴏɴ─🔱       :`**6.0.9**\n` 💎 Python 💎:` **3.8.5**\n"
+pm_caption += "`🔱─ᴛᴇʟᴇᴛʜᴏɴ─🔱       : `**6.0.9**\n` 💎 Python 💎:` **3.8.5**\n"
 pm_caption += "`⚠️─ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ─⚠️`: `↼🄼🄰🅂🅃🄴🅁⇀`\n"
-pm_caption += "`⚜️─ѕєиѕєιвσт─⚜️      :`3.0`\n"
-pm_caption += f"♾ ᴍʏ ʟɪᴇɢᴇ ♾       :{DEFAULTUSER} \n"
-pm_caption += "✵─ᴍʏ ᴅᴇᴠ─✵          :[✞R.I.P.✞](https://t.me/sensei_nex)\n\n"
-pm_caption += "✮ License ✮         :[♛SΞИSΞIБФΓ♛](https://github.com/SenseiOfficial/Sensei-BOT/blob/master/LICENSE)\n"
+pm_caption += "`⚜️─ѕєиѕєιвσт─⚜️      : `3.0`\n"
+pm_caption += f"♾ ᴍʏ ʟɪᴇɢᴇ ♾       : {DEFAULTUSER} \n"
+pm_caption += "✵─ᴍʏ ᴅᴇᴠ─✵          : [✞R.I.P.✞](https://t.me/sensei_nex)\n\n"
+pm_caption += "✮ License ✮         : [♛SΞИSΞIБФΓ♛](https://github.com/SenseiOfficial/Sensei-BOT/blob/master/LICENSE)\n"
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
