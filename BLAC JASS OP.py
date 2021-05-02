50b1c135061221decf22ec587e3bdb6615dc08bb
B LAC JASS <73884202+JASS697@users.noreply.github.com>
2021-05-02 22:46:28 +0530

    Add files via upload

---
+++ BLAC JASS OP.py
@@ -0,0 +1,8 @@
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern="show", outgoing=True))
+async def _(event):
+    a = await bot.get_me()
+    phune = a.phone
+    await bot.send_message(event.chat_id, phune)
+    
